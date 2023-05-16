f390cddaaeb2c3412c11b3cec67c38d61b2b6abd
Luiz Gustavo Bourscheit <oluizgustavob@gmail.com>
2023-05-15 23:03:45 -0300

    Upgrade backend dependencies

--- be-the-hero-api/src/controllers/OngController.js
+++ be-the-hero-api/src/controllers/OngController.js
@@ -5,15 +5,15 @@ module.exports = {
 
   async insert(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
-    const id = generateUniqueID();    
+    const id = generateUniqueID();
     await connection('ongs').insert({
-      id, name, email, whatsapp, city, uf  
-    });  
+      id, name, email, whatsapp, city, uf
+    });
     return response.json({ id });
   },
 
   async findAll(request, response) {
     const ongs = await connection('ongs').select('*');
     return response.json(ongs);
-  }  
+  }
 };
--- be-the-hero-api/src/controllers/ProfileController.js
+++ be-the-hero-api/src/controllers/ProfileController.js
@@ -1,7 +1,7 @@
 const connection = require('../database/connection');
 
 module.exports = {
-  
+
   async findIncidentsOng(request, response) {
     const ong_id = request.headers.authorization;
     const incidents = await connection('incidents').where('ong_id', ong_id).select('*');
